418c080e700fa4248af92a002707e421079856c9
IgnPine <ipineda@alumno.huergo.edu.ar>
2025-05-28 22:10:45 -0300

    Implementar la estructura del repositorio

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Pineda_Challenge_Gob
+---
+# Primera Instancia
+Implementé la estructura que va a tener el repositorio, diviendo backend y frontend, así como la base de un proyecto Angular y una API Rest con PHP.
---
+++ backend/index.php
@@ -0,0 +1,56 @@
+<?php
+if (isset($_GET["action"])){
+    switch($_SERVER["REQUEST_METHOD"]){
+        case "POST":
+            switch ($_GET["action"]){
+                case "Alta":
+                    if (isset($_POST['Nombre'], $_POST['DNI'], 
+                            $_POST['Fecha_Nac'], $_POST['Desarrollador'], 
+                            $_POST['Descripcion'], $_FILES['IdArea'])
+                        ) {
+                        include "empleadoAlta.php";
+                    }
+                    else{
+                        echo "Faltaron parametros";
+                    }
+                    break;
+                default:
+                    echo "Accion no incluida";
+                    break;
+            }
+            break;
+        case "GET":
+            switch ($_GET["action"]){
+                case "Consulta":
+                    include "ConsultasEmpleado.php";
+                    return ConsultasEmpleado::TraerListado();
+                    break;
+                default:
+                    break;
+                }
+            break;
+        case "PUT":
+            switch ($_GET["action"]){
+                case "Modificar":
+                    include "empleadoModificar.php";
+                    break;
+                default:
+                echo "No existe esa accion";
+                    break;
+            }
+            break;
+        case "DELTE":
+            switch ($_GET["action"]){
+                case "Borrar":
+                    include "empleadoBorrar.php";
+                    break;
+                default:
+                echo "No existe esa accion";
+                    break;
+            }
+            break;
+        default:
+            echo "No programado";
+            break;
+    }
+}
