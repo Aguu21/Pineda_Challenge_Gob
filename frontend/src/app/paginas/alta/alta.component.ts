f95ab8913b08713d1b83ca3ca80840ebc77cc1a2
Aguu21 <agustinpineda04@gmail.com>
2025-05-29 09:22:50 -0300

    Implementar diseño de '/alta'

--- frontend/src/app/paginas/alta/alta.component.ts
+++ frontend/src/app/paginas/alta/alta.component.ts
@@ -1,11 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-alta',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './alta.component.html',
   styleUrl: './alta.component.css'
 })
 export class AltaComponent {
+  constructor(private router: Router){}
 
+  empleado = {
+    nombre: "",
+    dni: 0,
+    fecha_nac: "",
+    desarrollador: false,
+    descripcion: "",
+    idArea: 0
+  }
+
+  volver(){
+    this.router.navigate(["/home"]);
+  }
+
+  registrar(){
+    return;
+  }
 }
